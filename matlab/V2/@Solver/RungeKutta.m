462bf781a184cc83c39d6d5e9cee69f860d8e0d4
revolt3245 <44830134+revolt3245@users.noreply.github.com>
2022-04-19 16:22:20 +0900

    add RungeKutta method to class Solver

--- matlab/V2/@Solver/RungeKutta.m
+++ matlab/V2/@Solver/RungeKutta.m
@@ -4,6 +4,6 @@ dx2 = Fcn(x_pre + dx1*self.dt/2);
 dx3 = Fcn(x_pre + dx2*self.dt/2);
 dx4 = Fcn(x_pre + dx3*self.dt);
 
-x_next = x_pre + (dx1 + dx2*2 + dx3*2 + dx4)/6;
+x_next = x_pre + (dx1 + dx2*2 + dx3*2 + dx4)*self.dt/6;
 
 end
